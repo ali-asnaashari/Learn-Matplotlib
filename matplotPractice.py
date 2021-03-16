b189d3bad9232fe891176699137995bf05038b3e
Ali Asnaashari <Ali.Asnaashari2000@gmail.com>
2021-03-16 20:58:34 +0330

    Part6 - xticks & grid

--- matplotPractice.py
+++ matplotPractice.py
@@ -53,6 +53,7 @@ plt.show()
 """
 
 # Part5
+"""
 x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 y = [2, 1, 6, 5, 6, 8, 2, 5, 4, 4]
 # create window
@@ -65,4 +66,23 @@ plt.title("my first plot \n Ali asnaashari", fontsize=13)
 plt.xlabel("x", fontsize=14)
 # ylabel --> latex form
 plt.ylabel("$y = x^2$", fontsize=14)
+plt.show()
+"""
+
+# Part6
+x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+y = [2, 1, 6, 5, 6, 8, 2, 5, 4, 4]
+# create window
+plt.figure(figsize=(10, 8))
+plt.plot(x, y, linewidth=4, color="blue", linestyle="--", marker="o", markersize=12, markerfacecolor="lightblue",
+         markeredgecolor="black")
+# title
+plt.title("my first plot \n Ali asnaashari", fontsize=13)
+# xlabel
+plt.xlabel("x", fontsize=14)
+# ylabel --> latex form
+plt.ylabel("$y = x^2$", fontsize=14)
+# Fully display the numbers on the x-axis
+plt.xticks(x)
+plt.grid(which="both")
 plt.show()
