ef3bf083c79ff87f6a17f77eee8a60fdf45048f5
Ali Asnaashari <Ali.Asnaashari2000@gmail.com>
2021-03-16 19:53:37 +0330

    part2 - figure and title

--- matplotPractice.py
+++ matplotPractice.py
@@ -1,7 +1,19 @@
 import matplotlib.pyplot as plt
 
 # Part1
+"""
 x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 y = [2, 1, 6, 5, 6, 8, 2, 5, 4, 4]
 plt.plot(x, y)
+plt.show()
+"""
+
+# Part2
+x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+y = [2, 1, 6, 5, 6, 8, 2, 5, 4, 4]
+# create window
+plt.figure(figsize=(10, 8))
+plt.plot(x, y)
+# title
+plt.title("my first plot \n Ali asnaashari", fontsize=13)
 plt.show()
