3de3fd938477345baa749887228a3777c7eba137
Ali Asnaashari <Ali.Asnaashari2000@gmail.com>
2021-03-16 22:01:42 +0330

    Part5 - legend another version

--- matplotPracticeV2.py
+++ matplotPracticeV2.py
@@ -37,6 +37,7 @@ plt.show()
 '''
 
 # Part4
+'''
 fig = plt.figure(figsize=(10, 8))
 plt.plot(x, y1)
 plt.plot(x, y2)
@@ -46,4 +47,17 @@ plt.xticks(np.arange(0, 2 * np.pi + 0.01, np.pi / 4),
            ['0', '$\pi/4$', '$\pi/2$', '$3\pi4/$', '$\pi$', '$5\pi/4$', '$3\pi/2$', '$7\pi/4$', '$2\pi$'])
 plt.yticks([-1, -0.5, 0, 0.5, 1])
 plt.legend(["sin(x)", "cos(x)"], fontsize=14)
+plt.show()
+'''
+
+# Part5
+fig = plt.figure(figsize=(10, 8))
+plt.plot(x, y1, label="sin(x)")
+plt.plot(x, y2, label="cos(x)")
+plt.xlabel("X")
+plt.ylabel("F(X)")
+plt.xticks(np.arange(0, 2 * np.pi + 0.01, np.pi / 4),
+           ['0', '$\pi/4$', '$\pi/2$', '$3\pi4/$', '$\pi$', '$5\pi/4$', '$3\pi/2$', '$7\pi/4$', '$2\pi$'])
+plt.yticks([-1, -0.5, 0, 0.5, 1])
+plt.legend(fontsize=14)
 plt.show()
