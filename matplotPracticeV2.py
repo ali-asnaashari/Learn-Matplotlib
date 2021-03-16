a1f08837eebebfdd327be8ce0e3988d235a8a099
Ali Asnaashari <Ali.Asnaashari2000@gmail.com>
2021-03-16 21:52:00 +0330

    Part3 - xticks and yticks

--- matplotPracticeV2.py
+++ matplotPracticeV2.py
@@ -14,9 +14,22 @@ plt.show()
 """
 
 # Part2
+"""
 fig = plt.figure(figsize=(10, 8))
 plt.plot(x, y1)
 plt.plot(x, y2)
 plt.xlabel("X")
 plt.ylabel("F(X)")
+plt.show()
+"""
+
+# Part3
+fig = plt.figure(figsize=(10, 8))
+plt.plot(x, y1)
+plt.plot(x, y2)
+plt.xlabel("X")
+plt.ylabel("F(X)")
+plt.xticks(np.arange(0, 2 * np.pi + 0.01, np.pi / 4),
+           ['0', '$\pi/4$', '$\pi/2$', '$3\pi4/$', '$\pi$', '$5\pi/4$', '$3\pi/2$', '$7\pi/4$', '$2\pi$'])
+plt.yticks([-1, -0.5, 0, 0.5, 1])
 plt.show()
