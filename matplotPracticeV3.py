bd7f554f6dbc2801ee6f06e7d7c04ecce9adac17
Ali Asnaashari <Ali.Asnaashari2000@gmail.com>
2021-03-18 18:40:20 +0330

    Draw Completed

--- matplotPracticeV3.py
+++ matplotPracticeV3.py
@@ -12,9 +12,27 @@ plt.show()
 '''
 
 # Part2
+'''
 fig = plt.figure(figsize=(15, 10))
 plt.bar(x, y)
 plt.xticks(range(1, 13), calendar.month_name[1:], fontsize=14, rotation=45)
 plt.yticks(fontsize=14)
 plt.ylabel("sales", fontsize=14)
+plt.show()
+'''
+
+# Part3
+fig = plt.figure(figsize=(15, 10))
+plot1 = plt.bar(x, y)
+plt.xticks(range(1, 13), calendar.month_name[1:], fontsize=14, rotation=45)
+plt.yticks(fontsize=14)
+plt.ylabel("sales", fontsize=14)
+for rect in plot1:
+    # print(rect)
+    height = rect.get_height()
+    width = rect.get_width()
+    xpos = rect.get_x()
+    color = rect.get_facecolor()
+    # ha = horizontal alignment
+    plt.text(xpos + width / 2, height * 1.01, f'{height}', fontsize=14, ha='center', color=color)
 plt.show()
